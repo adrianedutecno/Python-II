e1660eff2644639bb142ba5acaac7034d68d397a
afranco <adrian.franco@edutecno.com>
2023-05-17 21:30:49 -0400

    S6: rebound, drilling

--- 6-S6/drilling.py
+++ 6-S6/drilling.py
@@ -3,9 +3,9 @@ def validar_clave(diccionario,clave):
     mensaje = 'La clave 004 no está en el diccionario'
     try:#intenta hacer esto
         print(diccionario[clave])
-    except KeyError: #Oh no!
+    except KeyError: #Oh no!, se captura el error
         print(mensaje)
-    diccionario[clave]= None #le asignamos valor al error
+    diccionario[clave]= None #agregamos la clave y se le da el valor None
 
 #inicio de la ejecucion del codigo    
 usuarios = {'001':'Marca','002':'Mónica','003':'Jacob'}

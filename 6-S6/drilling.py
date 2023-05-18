882fc929dc9c0a2534527866da43c35123724a7e
afranco <adrian.franco@edutecno.com>
2023-05-17 21:27:41 -0400

    S6: rebound, drilling

--- 6-S6/drilling.py
+++ 6-S6/drilling.py
@@ -1,11 +1,15 @@
+#def, define la funcion
+def validar_clave(diccionario,clave):
+    mensaje = 'La clave 004 no está en el diccionario'
+    try:#intenta hacer esto
+        print(diccionario[clave])
+    except KeyError: #Oh no!
+        print(mensaje)
+    diccionario[clave]= None #le asignamos valor al error
+
+#inicio de la ejecucion del codigo    
 usuarios = {'001':'Marca','002':'Mónica','003':'Jacob'}
 id_usuario = '004'
-mensaje = 'La clave 004 no está en el diccionario'
-try:#intenta hacer esto
-    print(usuarios[id_usuario])
-
-except KeyError: #Oh no!
-    print(mensaje)
-usuarios[id_usuario]= None #le asignamos valor al error
+validar_clave(usuarios,id_usuario) #invocando a la funcion
 
 print(usuarios)
